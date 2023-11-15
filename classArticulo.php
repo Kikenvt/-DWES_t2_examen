43afe908dcff0f1a4d606e5d6d7d841462b33496
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-11-15 11:45:17 +0100

    Creada clase articulo

---
+++ classArticulo.php
@@ -0,0 +1,46 @@
+<?php
+
+class Articulo {
+    private $name;
+    private $cost;
+    private $price;
+    private $count;
+
+    public function __construct($name, $cost, $price, $count) {
+        $this->name = $name;
+        $this->cost = $cost;
+        $this->price = $price;
+        $this->count = $count;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+    public function getCost() {
+        return $this->cost;
+    }
+    public function getPrice() {
+        return $this->price;
+    }
+    public function getCount() {
+        return $this->count;
+    }
+    public function setName( $name ) {
+        $this->name = $name;
+    }
+    public function setCost( $cost ) {
+        $this->cost = $cost;
+    }
+    public function setPrice( $price ) {
+        $this->price = $price;
+    }
+    public function setCount( $count ) {
+        $this->count = $count;
+    }
+
+    public function __toString() {
+        return "Nombre: ". $this->name ."Coste :". $this->cost . "Precio: ". $this->price ."Contador: " . $this->count;
+    }
+}
+
+?>
